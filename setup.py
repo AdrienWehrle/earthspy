436ef87353b8d4f9afd8cd569d7e7d5a89de4ad1
Adrien Wehrlé <adrien.wehrle@hotmail.fr>
2022-02-18 14:08:27 +0100

    first basic setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.org').read_text(encoding='utf-8')
+
+setup(
+    name='sat-spy',
+    version='0.1.0',
+    description='Wrapper for sentinelhub-py services',
+    long_description=long_description,  
+    author='Adrien Wehrlé',
+    author_email='adrien.wehrle@hotmail.fr',
+    packages=find_packages(include=['earthspy', 'earthspy.*'])
+)
