9b8e462aebc8c43aa52c99f064224a876d5c2579
AdrienWehrle <adrien.wehrle@hotmail.fr>
2022-02-19 16:01:32 +0100

    blacken code

--- earthspy/earthspy.py
+++ earthspy/earthspy.py
@@ -162,8 +162,9 @@ class EarthSpy:
         self.get_store_folder(store_folder)
 
         if download_mode == "D":
-            self.split_boxes = [shb.BBox(bbox=self.bounding_box,
-                                         crs=shb.CRS.WGS84)]
+            self.split_boxes = [
+                shb.BBox(bbox=self.bounding_box, crs=shb.CRS.WGS84)
+            ]
         elif download_mode == "SM":
             self.get_split_boxes()
 
@@ -250,17 +251,21 @@ class EarthSpy:
         if isinstance(time_interval, int):
 
             today = datetime.today().strftime("%Y-%m-%d")
-            nb_days_back = (datetime.today() - timedelta(days=time_interval)).strftime(
-                "%Y-%m-%d"
-            )
+            nb_days_back = (
+                datetime.today() - timedelta(days=time_interval)
+            ).strftime("%Y-%m-%d")
             self.date_range = pd.date_range(nb_days_back, today)
 
         elif isinstance(time_interval, list):
 
             if len(time_interval) > 1:
-                self.date_range = pd.date_range(time_interval[0], time_interval[1])
+                self.date_range = pd.date_range(
+                    time_interval[0], time_interval[1]
+                )
             else:
-                self.date_range = pd.date_range(time_interval[0], time_interval[0])
+                self.date_range = pd.date_range(
+                    time_interval[0], time_interval[0]
+                )
 
         elif isinstance(time_interval, str):
             self.date_range = pd.date_range(time_interval, time_interval)
@@ -289,8 +294,9 @@ class EarthSpy:
 
         elif isinstance(bounding_box, str):
 
-            area_bounding_boxes = pd.read_csv("./area_bounding_boxes.csv",
-                                              index_col=1)
+            area_bounding_boxes = pd.read_csv(
+                "./area_bounding_boxes.csv", index_col=1
+            )
 
             self.bounding_box = area_bounding_boxes[bounding_box]
             self.crs = area_bounding_boxes["crs"]
@@ -394,11 +400,20 @@ class EarthSpy:
 
         except IndexError:
 
-            if np.sum(nb_xpixels < 2500) == 0 and np.sum(nb_ypixels < 2500) == 0:
+            if (
+                np.sum(nb_xpixels < 2500) == 0
+                and np.sum(nb_ypixels < 2500) == 0
+            ):
                 origin = "x and y"
-            elif np.sum(nb_xpixels < 2500) == 0 and np.sum(nb_ypixels < 2500) != 0:
+            elif (
+                np.sum(nb_xpixels < 2500) == 0
+                and np.sum(nb_ypixels < 2500) != 0
+            ):
                 origin = "x"
-            elif np.sum(nb_xpixels < 2500) != 0 and np.sum(nb_ypixels < 2500) == 0:
+            elif (
+                np.sum(nb_xpixels < 2500) != 0
+                and np.sum(nb_ypixels < 2500) == 0
+            ):
                 origin = "y"
 
             raise IndexError(
@@ -482,11 +497,19 @@ class EarthSpy:
 
         trial_split_boxes = np.arange(2, 100)
 
-        boxes_pixels_x = (dx / trial_split_boxes) / self.data_collection_resolution
-        boxes_pixels_y = (dy / trial_split_boxes) / self.data_collection_resolution
+        boxes_pixels_x = (
+            dx / trial_split_boxes
+        ) / self.data_collection_resolution
+        boxes_pixels_y = (
+            dy / trial_split_boxes
+        ) / self.data_collection_resolution
 
-        min_nb_boxes_x = int(trial_split_boxes[np.where(boxes_pixels_x <= 2500)[0][0]])
-        min_nb_boxes_y = int(trial_split_boxes[np.where(boxes_pixels_y <= 2500)[0][0]])
+        min_nb_boxes_x = int(
+            trial_split_boxes[np.where(boxes_pixels_x <= 2500)[0][0]]
+        )
+        min_nb_boxes_y = int(
+            trial_split_boxes[np.where(boxes_pixels_y <= 2500)[0][0]]
+        )
 
         return min_nb_boxes_x, min_nb_boxes_y
 
@@ -498,7 +521,7 @@ class EarthSpy:
           the initial bounding box when merged.
         :rtype: list
         """
-        nb_boxes_x, nb_boxes_y=self.get_optimal_box_split()
+        nb_boxes_x, nb_boxes_y = self.get_optimal_box_split()
 
         if self.verbose:
             print(
@@ -539,7 +562,9 @@ class EarthSpy:
 
         return self.evaluation_script
 
-    def get_evaluation_script(self, evaluation_script: Union[None, str]) -> None:
+    def get_evaluation_script(
+        self, evaluation_script: Union[None, str]
+    ) -> None:
         """_summary_
 
         :param evaluation_script: _description_
@@ -586,7 +611,9 @@ class EarthSpy:
 
         for loc_bbox in self.split_boxes:
 
-            loc_size = shb.bbox_to_dimensions(loc_bbox, resolution=self.resolution)
+            loc_size = shb.bbox_to_dimensions(
+                loc_bbox, resolution=self.resolution
+            )
 
             request = shb.SentinelHubRequest(
                 data_folder=self.store_folder,
@@ -640,18 +667,14 @@ class EarthSpy:
             with open(f"{folder}/request.json") as json_file:
                 request = json.load(json_file)
 
-            date = request["payload"]["input"]["data"][0]["dataFilter"]["timeRange"][
-                "from"
-            ][:10]
+            date = request["payload"]["input"]["data"][0]["dataFilter"][
+                "timeRange"
+            ]["from"][:10]
 
             if self.download_mode == "D":
-                new_filename = (
-                    f"{self.store_folder}/{date}_{self.data_collection_str}.tif"
-                )
+                new_filename = f"{self.store_folder}/{date}_{self.data_collection_str}.tif"
             elif self.download_mode == "SM":
-                new_filename = (
-                    f"{self.store_folder}/{date}_{i}_{self.data_collection_str}.tif"
-                )
+                new_filename = f"{self.store_folder}/{date}_{i}_{self.data_collection_str}.tif"
 
             os.rename(f"{folder}/response.tiff", new_filename)
 
@@ -679,11 +702,15 @@ class EarthSpy:
             self.outputs = {}
 
             with Pool(self.nb_cores) as p:
-                for date, output in p.map(self.sentinelhub_request, self.date_range):
+                for date, output in p.map(
+                    self.sentinelhub_request, self.date_range
+                ):
                     self.outputs[date] = output
 
         else:
-            self.outputs = [self.sentinelhub_request(date) for date in self.date_range]
+            self.outputs = [
+                self.sentinelhub_request(date) for date in self.date_range
+            ]
 
         self.rename_output_files()
 
