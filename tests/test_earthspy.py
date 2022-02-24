167fd5fde73441a7c4f9fa7907df9803cfe8b036
AdrienWehrle <adrien.wehrle@hotmail.fr>
2022-02-24 21:09:51 +0000

    tests on few first methods

--- tests/test_earthspy.py
+++ tests/test_earthspy.py
@@ -11,24 +11,11 @@ import sentinelhub as shb
 
 class TestEarthspy:
 
-    def test_init(self) -> None:
-        """Test successful auth.txt parsing and connection configuration.
-
+    def __init__(self) -> None:
+        """Assign some test variables.
         """
-        self.t = es.EarthSpy('./auth_test.txt')
-
-        assert self.t.CLIENT_ID == 'test_username'
-        assert self.t.CLIENT_SECRET == 'test_password'
-
-        assert isinstance(self.t.config, shb.config.SHConfig)
-        assert self.t.config.sh_client_id == 'test_username'
-
-        return None
-
-    def test_set_query_parameters(self) -> None:
-        """Test that direct attribute assignenent is successful.
-        """
-        s2_evalscript = """
+        
+        self.test_evalscript = """
         //VERSION=3
         function setup(){
           return{
@@ -46,19 +33,45 @@ class TestEarthspy:
 
         """
 
-        sentinel2_collection = "SENTINEL2_L2A"
+        self.test_collection = "SENTINEL2_L2A"
+
+        return None
+    
+    def test_init(self) -> None:
+        """Test auth.txt parsing and connection configuration.
+
+        """
+        self.t = es.EarthSpy('./auth_test.txt')
+
+        assert self.t.CLIENT_ID == 'test_username'
+        assert self.t.CLIENT_SECRET == 'test_password'
+
+        assert isinstance(self.t.config, shb.config.SHConfig)
+        assert self.t.config.sh_client_id == 'test_username'
+
+        return None
+
+    def test_set_query_parameters(self) -> None:
+        """Test direct attribute assignement.
+        """
         
         self.t.set_query_parameters(bounding_box=[-51.13, 69.204, -51.06, 69.225],
                                     time_interval=['2019-08-23'],
-                                    evaluation_script=s2_evalscript,
-                                    data_collection=sentinel2_collection,
+                                    evaluation_script=self.test_evalscript,
+                                    data_collection=self.test_collection,
                                     download_mode='SM')
         
         assert self.t.download_mode is not None
         assert self.t.verbose
-        assert self.t.data_collection == sentinel2_collection
+        assert self.t.data_collection == self.test_collection
 
         return None
 
-    def 
+    def test_get_data_collection(self) -> None:
+        """Test data collection selection.
+        """
+        self.t.get_data_collection()
+
+        assert self.t.data_collection == shb.DataCollection[self.test_collection]
         
+        return None
