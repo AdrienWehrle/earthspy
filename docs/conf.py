0288293c403140f53cbb77b070d6cc5001b7106c
AdrienWehrle <adrien.wehrle@hotmail.fr>
2022-05-02 21:54:35 +0200

    add autosummary

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,7 @@ extensions = [
     "sphinx.ext.duration",
     "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
